c777e6330f58275e5ffa251ca0323fdbd6a5aac2
kobewinona <vaanaktln@gmail.com>
2023-05-11 19:01:34 +0700

    refactor: change file structure

--- mesto-react/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      <p className="footer__copyright">&copy; 2023 Климкин Дима</p>
-    </footer>
-  );
-}
-
-export default Footer;
