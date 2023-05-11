08113aff21b84ce79a315c86cb1863fb956f47e8
kobewinona <vaanaktln@gmail.com>
2023-05-11 19:15:06 +0700

    refactor: change file structure

---
+++ mesto-react/src/components/Card.jsx
@@ -0,0 +1,25 @@
+function Card({card, onCardClick}) {
+  return (
+    <li>
+      <figure className="places__place grow">
+        <button className={`places__trash-button ${card.isOwner && 'places__trash-button_active'}`}
+                type="button" aria-label="Удалить."></button>
+        <img
+          className="places__place-photo"
+          src={card.link}
+          alt={card.name}
+          onClick={onCardClick}
+        />
+        <figcaption className="places__place-caption">
+          <p className="places__place-name">{card.name}</p>
+          <div className="places__like-container">
+            <button className="places__like-button" type="button" aria-label="Нравится."></button>
+            <p className="places__like-count">{card.likes.length}</p>
+          </div>
+        </figcaption>
+      </figure>
+    </li>
+  );
+}
+
+export default Card;
---
+++ mesto-react/src/components/Main.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import api from '../utils/Api';
+import Card from './Card';
+
+
+function Main(props) {
+  const [userAvatar, setUserAvatar] = React.useState('#');
+  const [isAvatarLoaded, setIsAvatarLoaded] = React.useState(false);
+  
+  const [userName, setUserName] = React.useState('');
+  const [userJob, setUserJob] = React.useState('');
+  const [userID, setUserID] = React.useState('');
+  
+  const [initialCards, setInitialCards] = React.useState([]);
+  
+  
+  React.useEffect(() => {
+    Promise.all([api.getUserInfo(), api.getInitialCards()])
+      .then(([{avatar, name, about, _id}, initialCards]) => {
+        setUserAvatar(avatar);
+        setIsAvatarLoaded(!isAvatarLoaded);
+        
+        setUserName(name);
+        setUserJob(about);
+        setUserID(_id);
+        
+        setInitialCards(initialCards);
+      }).catch(err => console.log(err));
+  }, []);
+  
+  
+  return (
+    <main className="content">
+      <section className="profile">
+        <div className="profile__avatar-container">
+          <img
+            className={`profile__avatar ${isAvatarLoaded && 'profile__avatar_displayed'}`}
+            src={userAvatar} alt="Аватар."/>
+          <button
+            className="profile__edit-avatar-button"
+            onClick={props.onEditAvatar}>
+          </button>
+        </div>
+        <div className="profile__container">
+          <h1 className="profile__name">{userName}</h1>
+          <button
+            className="profile__edit-profile-button"
+            type="button"
+            aria-label="Редактировать профиль."
+            onClick={props.onEditProfile}>
+          </button>
+          <p className="profile__job">{userJob}</p>
+        </div>
+        <button
+          className="profile__add-place-button"
+          type="button"
+          aria-label="Добавить фотографию места."
+          onClick={props.onAddPlace}>
+        </button>
+      </section>
+      <section className="places" aria-label="Секция с фотографиями мест России.">
+        <ul className="places__list">
+          {initialCards.map((card) => {
+            return <Card
+              key={card._id}
+              card={card}
+              isOwner={card.owner._id === userID}
+              onCardClick={() => props.onCardClick(card)}/>;
+          })}
+        </ul>
+      </section>
+    </main>
+  );
+}
+
+export default Main;
